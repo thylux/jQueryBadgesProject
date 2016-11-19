8c7544f9dba63f26139d41ff6a846d0928578321
slazevedo <Sergio.Azevedo@i2s.pt>
2016-11-19 16:19:53 +0000

    The Build

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+      url: 'https://www.codeschool.com/users/924450.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        // handle response
+        response.courses.completed.forEach(elem => {
+          $('#badges').append(`<div class='course'>
+            <h3>${elem.title}</h3>
+            <img src='${elem.badge}'/>
+            <a href='${elem.url}' target='_blank' class='btn btn-primary'>See Course</a>
+          </div>`)
+        });
+      }
+    });
 });
